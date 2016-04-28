8ee867978af691a35a4d1deb3b96d4e638f74d9a
Jobin Kurian <ti.jobinkurian@gmail.com>
2016-04-28 15:38:52 +0530

    minor formating fixes.

--- jk-json-generator.js
+++ jk-json-generator.js
@@ -11,10 +11,12 @@ var output_dir = "json_output";
 var output_extenstion = ".json";
 var template_extenstion = ".jk";
 
+// Checking if the input directory exist.
 if (!fs.existsSync(input_dir)){
     console.log("\n\nCould not find the templates folder. \n\nPlease create folder named '" + input_dir + "' and add some templates with '" + template_extenstion + "' extention for converstion.\n\n");
     process.exit(1);
 }
+// Checking and creating output directory.
 if (!fs.existsSync(output_dir)){
     fs.mkdirSync(output_dir);
 }
@@ -22,34 +24,36 @@ if (!fs.existsSync(output_dir)){
 // Getting all the files inside the input directory.
 var all_files_array = getAllTemplateFiles(input_dir);
 
-// Taversing through the input_dir
+// Taversing through the input_dir.
 all_files_array.forEach(function (templateFile, index) {
 
     var	template_file_data = fs.readFileSync(templateFile, {encoding : 'utf8'});
-	var converted_JSON_text = dummyjson.parse(template_file_data);
+	var generated_JSON_data = dummyjson.parse(template_file_data);
 
     // Checking if there is an error in the generated JSON.
     // If there is error that means the original template is having error.
     try {
-        jsonlint.parse(converted_JSON_text);
+        // Validating the generated J
+        jsonlint.parse(generated_JSON_data);
 
-        writeToFile(templateFile, converted_JSON_text);
+        writeToFile(templateFile, generated_JSON_data);
+
         console.log('Converting %d/%d', index + 1, index + 1);
         if (all_files_array.length - 1 == index) {
             console.log("Completed Converting All Files To JSON");
         };
 
     } catch (err) {
+
         console.log("\n\nThere is an error in the template '" + templateFile + "'\nPlease correct the below error and re-run the converstion");
         console.log(err + "\n\n");  
-    
     }    
 });
 
 // Function to file the json data to the file
-function writeToFile(templateFile, converted_JSON_text) {
+function writeToFile(templateFile, generated_JSON_data) {
 
-        // This will remove the filename 
+        // This will remove the filename and give back the path without the file.
         var path_without_file = path.dirname(templateFile);
         var parentFolder = null;
 
@@ -76,16 +80,17 @@ function writeToFile(templateFile, converted_JSON_text) {
         var output_filepath = output_folderpath + path.basename(templateFile, template_extenstion) + output_extenstion;
 
         // Writing the JSON data to the file.
-        fs.writeFile(output_filepath, converted_JSON_text, function(err) {
+        fs.writeFile(output_filepath, generated_JSON_data, function(err) {
            if(err) {
-        	  console.log("Error ");
-        	   return console.log(err);
+        	   console.log("Error in writing to a file " +err);
+        	   return;
         }
 	});
 }
 
 // Function to get all the templates inside the input directory
-function getAllTemplateFiles (dir, files_){
+function getAllTemplateFiles (dir, files_) {
+
     files_ = files_ || [];
     var files = fs.readdirSync(dir);
     for (var i in files){
@@ -98,10 +103,12 @@ function getAllTemplateFiles (dir, files_){
             }
         }
     }
+
     if (files_.length == 0) {
     	console.log("\n\nThe Template Folder Seems To Be Empty :( \n\nPlease add some templates with '" + template_extenstion + "' extention for converstion.\n\n");
         process.exit(1);
     };
+
     return files_;
 }
 
